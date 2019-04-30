b9483b1bc0760ed3fef55787027731e561e65483
ESD <esd@ece-07teki-imac.local>
2019-04-30 14:02:50 +0800

    Initial Commit

---
+++ iBeacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeacon
+//
+//  Created by ESD 07 on 2019/4/30.
+//  Copyright © 2019年 0760230. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
